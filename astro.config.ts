5f16dcaf998b16ad493e33055a154efbf325f2f7
moatkon <moatkon@gmail.com>
2025-08-20 23:56:50 +0800

    favicon.ico

--- astro.config.ts
+++ astro.config.ts
@@ -34,10 +34,10 @@ export default defineConfig({
 		webmanifest({
 			// See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
 			name: siteConfig.title,
-			short_name: "Astro_Cactus", // optional
+			short_name: "Moatkon", // optional
 			description: siteConfig.description,
 			lang: siteConfig.lang,
-			icon: "public/icon.svg", // the source for generating favicon & icons
+			icon: "public/favicon.ico", // the source for generating favicon & icons
 			icons: [
 				{
 					src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
