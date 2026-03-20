94f1912262ca5548eb02179877c3e425a6aebaf9
moatkon <moatkon@gmail.com>
2026-03-20 14:11:49 +0800

    moatkon.com commit 1773987109

--- install_all.sh
+++ install_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Blog Install"
+echo ">>>>>Blog Install"
 npm install
 
 
-echo "CMS Server Install"
+echo ">>>>>CMS Server Install"
 cd cms
 npm install
 
-echo "CMS Client Install"
+echo ">>>>>CMS Client Install"
 cd client
 npm install
